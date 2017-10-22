e8f1b6c5554bb1c0e81b88b40ded6d38cdf6646d
Jorge Alberto Arreola Martinez <arreola@Jorges-MacBook-Pro.local>
2017-10-22 18:00:59 -0500

    it doesnt works

--- sites/inspiration/js/sections/categories.js
+++
@@ -1,5 +0,0 @@
-// HEADER LINK:ACTIVE IF URL HAS...
-var sectActive = $(".text-image-center").text();
-if($(".text-image-center").length > 0 ) {
-	$(".nav .blueDots-link a:contains("+sectActive+")").addClass("active");
-}
