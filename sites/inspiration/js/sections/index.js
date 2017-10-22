f6c677da02c5eff0261f35cc33d1ed41a17617eb
Jorge Alberto Arreola Martinez <arreola@Jorges-MacBook-Pro.local>
2017-10-22 17:30:34 -0500

    ajax

--- sites/inspiration/js/sections/index.js
+++ sites/inspiration/js/sections/index.js
@@ -1,39 +1,10 @@
-$(document).ready(function(){
-  $('#section01 .slider').slick({
-   	arrows: false,
-   	 responsive: [
-    {
-      breakpoint: 767,
-      settings: {
-      	dots: true,
-      }
-  	}
-
-  	]
-  });
-  $("#section01 .controls .slide-left").click(function(){
-  	$("#section01 .slider").slick('slickPrev');
-  })
-  $("#section01 .controls .slide-right").click(function(){
-  	$("#section01 .slider").slick('slickNext');
-  })
-  $(".close-pop").click(function(){
-    $(".share-mobile-pop").fadeOut();
-  });
-  $(".share-mobile").click(function(){
-    $(this).parent().next().fadeIn();
-    $(this).parent().next().next().fadeIn();
-  });
-
-  // Click fuera de header * cierra el menu
-  $('.share-mobile-pop, .share-mobile').on('click', function(e) {
-    e.stopPropagation();
-  });
-
-  $(document).on('click', function (e) {
-    $(".share-mobile-pop").fadeOut();
-  
-  });
-
-
-});
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "demo_get.asp", true);
+  xhttp.send();
+}
