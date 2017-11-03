fd5de9022a33f4f5fe4fd95a4c41a5a1de20ce5b
Jorge Arreola <j.arreolamtz@gmail.com>
2017-11-03 16:21:12 -0600

    about and contact

--- sites/photo-temp/js/sections/about.js
+++ sites/photo-temp/js/sections/about.js
@@ -1,9 +1,7 @@
-
-
 function checkOffset() {
-  if($('.wordmark-div').offset().top + $('.wordmark-div').height() >= $('#section-02').offset().top - 10)
+  if($('.wordmark-div').offset().top + $('.wordmark-div').height() >= $('#section-01').height() - 10)
       $('.wordmark-div').addClass("unActive");
-  if($(document).scrollTop() + window.innerHeight < $('#section-02').offset().top)
+  if($(document).scrollTop() + window.innerHeight < $('#section-01').height())
       // restore when you scroll up
       $('.wordmark-div').removeClass("unActive");
 }
