1784c63c75cb9d65596a250ff06a364674f12bc7
Jorge Arreola <j.arreolamtz@gmail.com>
2018-04-18 00:01:32 -0500

    Photo temp renew

---
+++ sites/photo-temp/js/plugins/cursor.js
@@ -0,0 +1,49 @@
+function home() {
+  follower = $(".cursor-follower"),
+  cursor = $(".cursor"),
+  cursorfollow(),
+  setTimeout(function() {
+    split_text("p")
+  }, 200),
+  setTimeout(function() {
+    $(".hide").removeClass("hide")
+  }, 500),
+  $(document).on("mousemove", function(e) {
+    mouseX = e.pageX,
+    mouseY = e.pageY,
+    0 == moved && (moved = !0, follower.removeClass("hide"), cursor.removeClass("hide"))
+  })
+}
+function cursorfollow() {
+  var e = 0,
+    s = 0;
+  setInterval(function() {
+    e += (mouseX - e) / 9,
+    s += (mouseY - s) / 9,
+    follower.css({
+      left: e - 12,
+      top: s - 12
+    }),
+    cursor.css({left: mouseX, top: mouseY})
+  }, 16);
+  $(".cur, a, .icon-menu, input, textarea, #section-02 .controls .box.mid, #section-02 .controls .box .image.mid").on("mouseenter", function() {
+    follower.addClass("active"),
+    cursor.addClass("active"),
+    $(".hover").addClass("hovered"),
+    $(this).removeClass("hovered")
+  }),
+  $(".cur, a, .icon-menu, input, textarea, #section-02 .controls .box.mid, #section-02 .controls .box .image.mid").on("mouseleave", function() {
+    follower.removeClass("active"),
+    cursor.removeClass("active"),
+    $(".hover").removeClass("hovered")
+  })
+  $(".fill-dots-blue").on("mouseenter", function() {
+    cursor.addClass("circleBlue"),
+    $(".hover").addClass("hovered"),
+    $(this).removeClass("hovered")
+  }),
+  $(".fill-dots-blue").on("mouseleave", function() {
+    cursor.removeClass("circleBlue"),
+    $(".hover").removeClass("hovered")
+  })
+}
--- sites/photo-temp/js/sections/index.js
+++ sites/photo-temp/js/sections/index.js
@@ -1,13 +1,14 @@
 
-
-function checkOffset() {
-  if($('.wordmark-div').offset().top + $('.wordmark-div').height() >= $('#section-02').offset().top - 10)
-      $('.wordmark-div').addClass("unActive");
-  if($(document).scrollTop() + window.innerHeight < $('#section-02').offset().top)
-      // restore when you scroll up
-      $('.wordmark-div').removeClass("unActive");
-}
-$(document).scroll(function() {
+$(document).ready(function(){
+  function checkOffset() {
+    if($('.wordmark-div').offset().top + $('.wordmark-div').height() >= $('#section-02').offset().top - 10)
+    $('.wordmark-div').addClass("unActive");
+    if($(document).scrollTop() + window.innerHeight < $('#section-02').offset().top)
+    // restore when you scroll up
+    $('.wordmark-div').removeClass("unActive");
+  }
+  $(document).scroll(function() {
     checkOffset();
+  });
+  checkOffset();
 });
-checkOffset();
